a22e851f4d228fbdfd2b61f5b8a040853e04a575
Vincent Ang <vinceajy93@gmail.com>
2023-01-10 14:21:16 +0800

    clean up

--- src/day04/Server.java
+++ src/day04/Server.java
@@ -1,19 +1,12 @@
 package day04;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.Reader;
-import java.io.Writer;
 import java.net.ServerSocket;
 import java.net.Socket;
-
-import javax.management.openmbean.OpenType;
 
 public class Server {
     public static void main(String[]args) throws IOException{
